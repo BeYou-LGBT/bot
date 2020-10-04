66a3aca65846b523d3c0c315d7b231e73c71fd4c
WeeskyBDW <bdwadv@gmail.com>
2020-10-04 03:49:32 +0200

    :wrench: Add possibility to config a value with name equivalent
    Remove unused config values

--- config/config.js
+++ config/config.js
@@ -1,17 +1,17 @@
 const env = process.argv[2] ? process.argv[2] : "dev"
 
-const config = env == "prod" ? require("config/config.prod") : require("config/config.dev")
+const config = env == "prod" ? require("config/config.json") : require("config/config.dev")
 
 function init(client) {
     config.server = client.guilds.cache.get(config.server)
     for (let i in config.channels) {
-        config.channels[i] = client.channels.cache.get(config.channels[i])
+        config.channels[i] = client.channels.cache.find(channel => channel.name == config.channels[i] || channel.id == config.channels[i])
         if (!config.channels[i]) console.log(`Impossible de récupéré ${i} !`)
     }
 
 
     for (let i in config.roles) {
-        config.roles[i] = config.server.roles.cache.get(config.roles[i])
+        config.roles[i] = config.server.roles.cache.find(role => role.name == config.roles[i] || role.id == config.roles[i])
         if (!config.roles[i]) console.log(`Impossible de récupéré ${i} !`)
     }
 }
