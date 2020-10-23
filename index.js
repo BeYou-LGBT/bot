1ef3a548ea71953ade83810c13874757645d3dd5
WeeskyBDW <bdwadv@gmail.com>
2020-10-23 03:33:36 +0200

    :art: Improve index file structure
    Split functions loadCommands and SQLTestConnexion to setup.js file

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
 const Discord = require('discord.js');
-const { MessageEmbed } = require('discord.js')
-const fs = require('fs');
 const config = require("config/config")
 const login = require('./config/login')
 Client = new Discord.Client();
-let Commandes = new Array();
-const Categories = new Map()
-const Path = require("path")
 Client.login(login.token).catch(e => console.log(`Erreur lors de la connexion à Discord: ${e}`))
 
-const sql = require("./Utils/sql")
-sql.SQLTestConnexion()
+
 console.log("Chargement des commandes");
-let i = 0;
-
-function loadcommandes(path) {
-    fs.readdir(path, (erreur, fichiers) => {
-        if (erreur) console.log(`Erreur lors de la lecture des commandes: ${erreur}`)
-        fichiers.forEach(function (fichier) {
-            if (fs.statSync(`${path}/${fichier}`).isDirectory()) return loadcommandes(`${path}/${fichier}`)
-            if (fichier.split(".").pop() == "js") {
-                Commandes.push(`${path}/${fichier}`)
-                i++
-
-                //chargement des infos pour la commande help
-                const parentFile = Path.resolve(path, fichier).toString().replace("\\" + fichier, "").split("\\").pop()
-                if(Categories.get(parentFile) == undefined) Categories.set(parentFile, "")
-                Categories.set(parentFile, Categories.get(parentFile) + " `" + fichier.slice(0, -3) + "`")
-
-            }
-        })
-    })
-
-}
-loadcommandes('./commandes')
-
+const setup = require("./setup.js")
+setup.loadCommands('./commandes')
+setup.SQLTestConnexion()
 Client.on('ready', function(){
     config.init(Client)
     require("./Events/ready").init(Client)
@@ -61,37 +35,3 @@ Client.on('messageDelete', msg => {
     require("./Events/messageDelete").log(msg)
 })
 
-Client.on('messageReactionAdd', (reaction, user) => {
-    if (reaction.message.id === config.messages.access && reaction.emoji.name === '✅') {
-        if (!reaction.message.member.roles.cache.has(config.roles.membre)) {
-            reaction.message.member.roles.add(config.roles.membre).catch(e => console.log(`Impossible de donner le role <@${config.roles.membre.id}> à <@${reaction.message.member.id}>`
-            + `\n ${e}`))
-        }
-    }
-    if (reaction.message.id == config.messages.help_noctif_role && reaction.emoji.name == '🔔') {
-        if (!reaction.message.member.roles.cache.has(config.roles.help_noctif)) {
-            reaction.message.member.roles.add(config.roles.help_noctif).catch(e => console.log(`Impossible de donner le role <@${config.roles.membre.id}> à <@${reaction.message.member.id}>`
-                + `\n ${e}`))
-        }
-    }
-})
-
-Client.on('messageReactionRemove', (client, data, reaction) => {
-    //reaction.message.channel.send(reaction.emoji.name)
-    if (reaction.message.id == config.messages.access && reaction.emoji.name == '✅') {
-        if (reaction.message.member.roles.cache.has(config.roles.membre)) {
-            reaction.message.member.roles.add(config.roles.membre).catch(e => console.log(`Impossible de donner le role <@${config.roles.membre.id}> à <@${reaction.message.member.id}>`
-                + `\n ${e}`))
-        }
-    }
-    if (reaction.message.id == config.messages.help_noctif_role && reaction.emoji.name == '🔔') {
-        if (!reaction.message.member.roles.has(config.roles.help_noctif)) {
-            reaction.message.member.roles.add(config.roles.help_noctif).catch(e => console.log(`Impossible de donner le role <@${config.roles.membre.id}> à <@${reaction.message.member.id}>`
-                + `\n ${e}`))
-        }
-    }
-})
-
-module.exports.Commandes = Commandes;
-module.exports.Categories = Categories;
-module.exports.Client = Client;
